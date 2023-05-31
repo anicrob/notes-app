30fab4e8a8dd66b3bef22ada7bf89f6411d6d5a9
goose <anrobbins18@gmail.com>
2023-05-31 16:28:08 -0400

    restructured code to work with promises

--- routes/index.js
+++ routes/index.js
@@ -8,37 +8,22 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const util = require('util');
 
-//defining readFromFile
-const readFromFile = util.promisify(fs.readFile);
-
-//defining writeToFile
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
-  
-  //defining readAndAppend
-  const readAndAppend = (content, file) => {
-    fs.readFile(file, 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        const parsedData = JSON.parse(data);
-        parsedData.push(content);
-        writeToFile(file, parsedData);
-      }
-    });
-}
-
 //creating instance of express
 const app = express();
 
+//promisify the fs functions
+const readFromFile = util.promisify(fs.readFile);
+const writeToFile = util.promisify(fs.writeFile);
+
 // GET route
 app.get('/notes', (req, res) => {
-    readFromFile('./db/db.json').then((data) =>
-    res.json(JSON.parse(data))
-  );}
-);
+    readFromFile('./db/db.json')
+        .then(notes => res.json(JSON.parse(notes)))
+        .catch((err) => {
+            res.status(500)
+            .json(err)
+        })
+    });
 
 // POST route
 app.post('/notes', (req, res) => {
@@ -51,28 +36,45 @@ app.post('/notes', (req, res) => {
         text,
         id: uuidv4(),
     }
-    if(newNote.title){
-        readAndAppend(newNote, './db/db.json');
-        res.json('note was successfully added');
-    } 
-}
+
+    readFromFile('./db/db.json')
+        .then((notes) => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes))
+            } catch (error) {
+                parsedNotes = []
+            }
+            return parsedNotes
+        })
+        .then(parsedNotes => [...parsedNotes, newNote])
+        .then(updatedNotes => writeToFile('./db/db.json', JSON.stringify(updatedNotes)))
+        .then(updatedNotes => res.status(200).json(updatedNotes))
+        .catch((err) => {
+            res.status(500)
+            .json(err)
+        })
+    }
 );
 
 // DELETE route
 app.delete('/notes/:id', (req, res) => {
-
-    const trashNote = {
-        id: req.params.id,
-    }
-
-    readFromFile('./db/db.json', (err, data) => {
-        if(err) throw err;
-        for(let i = 0; i < data.length; i++ ){
-            if(data[i].id === trashNote.id){
-                const updatedData = data.splice(i, 1);
-                writeToFile('./db/db.json', updatedData);
-            }
+    readFromFile('./db/db.json')
+    .then((notes) => {
+        let parsedNotes;
+        try {
+            parsedNotes = [].concat(JSON.parse(notes))
+        } catch (error) {
+            parsedNotes = []
         }
+        return parsedNotes
+    })    
+    .then((notes) => notes.filter(note => note.id !== req.params.id))
+    .then(updatedNotes => writeToFile('./db/db.json', JSON.stringify(updatedNotes)))
+    .then(() => res.json({ok: true}))
+    .catch((err) => {
+        res.status(500)
+        .json(err)
     })
 })
 
