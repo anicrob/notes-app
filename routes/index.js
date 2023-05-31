a2a595ad108927d3b5eedbbb7c9a523729e4cda0
goose <anrobbins18@gmail.com>
2023-05-31 16:45:40 -0400

    added comments

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,11 @@ const writeToFile = util.promisify(fs.writeFile);
 
 // GET route
 app.get('/notes', (req, res) => {
+    //read the file
     readFromFile('./db/db.json')
+        //send response of the parsed data
         .then(notes => res.json(JSON.parse(notes)))
+        //catch errors
         .catch((err) => {
             res.status(500)
             .json(err)
@@ -27,17 +30,17 @@ app.get('/notes', (req, res) => {
 
 // POST route
 app.post('/notes', (req, res) => {
-    console.log(req.body);
-
+    //destructured the request body
     const { title, text } = req.body;
-
+    //create a new note object with a unique id
     const newNote = {
         title,
         text,
         id: uuidv4(),
     }
-
+    //read the file first
     readFromFile('./db/db.json')
+    //make the data an array and return it
         .then((notes) => {
             let parsedNotes;
             try {
@@ -47,9 +50,13 @@ app.post('/notes', (req, res) => {
             }
             return parsedNotes
         })
+        //then use the spread array method to join the parsedNotes read from the file with the new note
         .then(parsedNotes => [...parsedNotes, newNote])
+        //then write those updated notes to the file
         .then(updatedNotes => writeToFile('./db/db.json', JSON.stringify(updatedNotes)))
+        //then send a response of the status and updated notes
         .then(updatedNotes => res.status(200).json(updatedNotes))
+        //catch any errors 
         .catch((err) => {
             res.status(500)
             .json(err)
@@ -59,7 +66,9 @@ app.post('/notes', (req, res) => {
 
 // DELETE route
 app.delete('/notes/:id', (req, res) => {
+    //read from file
     readFromFile('./db/db.json')
+    //make data from file an array and return it
     .then((notes) => {
         let parsedNotes;
         try {
@@ -69,13 +78,18 @@ app.delete('/notes/:id', (req, res) => {
         }
         return parsedNotes
     })    
+    //then filter out notes where the params id and note.id do not match
     .then((notes) => notes.filter(note => note.id !== req.params.id))
+    //then write that new data to the file
     .then(updatedNotes => writeToFile('./db/db.json', JSON.stringify(updatedNotes)))
+    //it was successful/ok
     .then(() => res.json({ok: true}))
+    //catch errors
     .catch((err) => {
         res.status(500)
         .json(err)
     })
 })
 
+//export app
 module.exports = app;
